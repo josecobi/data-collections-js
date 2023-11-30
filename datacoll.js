8f57415174c8f5d9af21f1d40a5128d5a3925eae
josecobi <58313777+josecobi@users.noreply.github.com>
2023-11-30 18:54:04 -0500

    Fixed issue with last row. Removed unnecessary loop and variables

--- datacoll.js
+++ datacoll.js
@@ -2,21 +2,6 @@ console.log("\n>>>>>>>>>>>>>>>>>>>>Part 2: Refactoring Old Code<<<<<<<<<<<<<<<<<
 
 const csvString = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26";
 
-let newLineCounter = 0;
-let commaCounter = 0;
-
-for (let k of csvString) {
-    if (k === '\n') {
-        newLineCounter++;
-    }
-    if (k === ',' && newLineCounter === 0) {
-        commaCounter++;
-    }
-}
-
-let numOfRows = newLineCounter + 1;
-let numOfColumns = commaCounter + 1;
-
 
 
 let rows = [];
@@ -36,5 +21,9 @@ for (let k of csvString) {
         cell.push(k);
     }
 }
+
+
+row.push(cell.join(''));
+rows.push(row);
 
 console.log(rows);
